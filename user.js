dea94eb5716ed6b23559c0138a22d8476ee8530e
ThePillenwerfer <42588539+ThePillenwerfer@users.noreply.github.com>
2026-02-16 01:22:43 +0000

    Add files via upload

---
+++ user.js
@@ -0,0 +1,21 @@
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.backspace_action", 0);
+user_pref("browser.mailto.prompt.os", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("browser.newtabpage.activity-stream.weather.staticData.enabled", false);
+user_pref("browser.newtabpage.activity-stream.weather.temperatureUnits", "f");
+user_pref("browser.privatebrowsing.felt-privacy-v1",false);
+user_pref("browser.sessionstore.resume_from_crash",	false);	
+user_pref("browser.theme.toolbar-theme", 1);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.toolbars.bookmarks.visibility", always);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", true)
+user_perf("layout.spellcheckDefault", 2);
+user_pref("network.protocol-handler.external.mailto", false);
+user_pref("sidebar.revamp",false);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("widget.gtk.overlay-scrollbars.enabled", false);
+user_pref("widget.non-native-theme.gtk.scrollbar.allow-buttons", true);
+user_pref("widget.non-native-theme.scrollbar.size.override", 16);
+
